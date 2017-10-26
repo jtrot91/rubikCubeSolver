1b27866a4dea35aeb39604d09c4ebe3dc6513dbf
James <jtrot91@bellsouth.net>
2017-10-26 02:22:43 -0400

    Some turns
    Top and bottoms

--- cubeSolver.py
+++ cubeSolver.py
@@ -1,12 +1,21 @@
+clockwise = [6, 3, 0, 7, 4, 1, 8, 5, 2]
+counterClockwise = [2, 5, 8, 1, 4, 7, 0, 3, 6]
 
 class Cube:
+	
 	def __init__(self):
-		self.front = ['R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R']
+		'''self.front = ['R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R']
 		self.top = ['Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y']
 		self.bottom = ['W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W']
 		self.left = ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B']
 		self.right = ['G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G']
-		self.back = ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']
+		self.back = ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']'''
+		self.front = ['B', 'G', 'G', 'O', 'R', 'B', 'R', 'W', 'O']
+		self.top = ['Y', 'G', 'R', 'B', 'Y', 'Y', 'Y', 'W', 'R']
+		self.bottom = ['W', 'R', 'W', 'W', 'W', 'Y', 'W', 'B', 'Y']
+		self.left = ['O', 'W', 'O', 'G', 'B', 'G', 'O', 'O', 'B']
+		self.right = ['Y', 'R', 'W', 'Y', 'G', 'R', 'B', 'O', 'R']
+		self.back = ['G', 'R', 'G', 'B', 'O', 'Y', 'B', 'O', 'G']
 	
 	def printCube(self):
 		print("       "+self.left[0]+"|"+self.left[1]+"|"+self.left[2])
@@ -30,18 +39,86 @@ class Cube:
 		   RRR		      210
 		'''
 	'''def frontTurn(self):
+		tempFront = self.front
+		tempTop = self.top
+		tempLeft = self.left
+		tempBottom = self.bottom
+		tempRight = self.right
+		
+		for x in range(0,3):
+			self.top[x] = 
 	def frontTurnPrime(self):
 	def leftTurn(self):
 	def leftTurnPrime(self):
 	def rightTurn(self):
 	def rightTurnPrime(self):
 	def backTurn(self):
-	def backTurnPrime(self):
+	def backTurnPrime(self):'''
 	def topTurn(self):
+		tempTop = self.top[:]
+		tempFront = self.front[:]
+		tempLeft = self.left[:]
+		tempBack = self.back[:]
+		tempRight = self.right[:]
+		
+		for x in range(0,3):
+			self.front[x] = tempRight[x]
+			self.left[x] = tempFront[x]
+			self.back[x] = tempLeft[x]
+			self.right[x] = tempBack[x]
+			
+		for x in range(0,9):
+			self.top[x] = tempTop[clockwise[x]]
+			
 	def topTurnPrime(self):
+		tempTop = self.top[:]
+		tempFront = self.front[:]
+		tempLeft = self.left[:]
+		tempBack = self.back[:]
+		tempRight = self.right[:]
+		
+		for x in range(0,3):
+			self.front[x] = tempLeft[x]
+			self.left[x] = tempBack[x]
+			self.back[x] = tempRight[x]
+			self.right[x] = tempFront[x]
+			
+		for x in range(0,9):
+			self.top[x] = tempTop[counterClockwise[x]]
 	def bottomTurn(self):
-	def bottomTurnPrime(self):'''
+		tempBottom = self.bottom[:]
+		tempFront = self.front[:]
+		tempLeft = self.left[:]
+		tempBack = self.back[:]
+		tempRight = self.right[:]
+		
+		for x in range(6,9):
+			self.front[x] = tempLeft[x]
+			self.left[x] = tempBack[x]
+			self.back[x] = tempRight[x]
+			self.right[x] = tempFront[x]
+			
+		for x in range(0,9):
+			self.bottom[x] = tempBottom[clockwise[x]]
+	def bottomTurnPrime(self):
+		tempBottom = self.bottom[:]
+		tempFront = self.front[:]
+		tempLeft = self.left[:]
+		tempBack = self.back[:]
+		tempRight = self.right[:]
+		
+		for x in range(6,9):
+			self.front[x] = tempRight[x]
+			self.left[x] = tempFront[x]
+			self.back[x] = tempLeft[x]
+			self.right[x] = tempBack[x]
+			
+		for x in range(0,9):
+			self.bottom[x] = tempBottom[counterClockwise[x]]
 	
 		
 cube = Cube()
+cube.printCube()
+cube.bottomTurnPrime()
+print("")
 cube.printCube()
